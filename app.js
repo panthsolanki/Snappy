2653b8fc5292431f961c4dbd8ffb975de81ff2f8
panth.solanki <panth.solanki@gmail.com>
2017-01-29 17:06:33 +0530

    Static with js basic

---
+++ app.js
@@ -0,0 +1,38 @@
+click("signin-button", logInUser);
+//object in js
+var user1 = {
+     id: 1,
+     name: "Panth",
+     username: "panthsolanki",
+     sayHello: function(){
+          console.log("Hello " + this.name);
+     }
+};
+
+var user2 = {
+     id: 2,
+     name: "neo",
+     username: "neo_da_leo",
+     sayHello: function(){
+          console.log("Hello " + this.name);
+     }
+};
+
+//Array in js
+var users = [
+     user1,
+     user2
+];
+
+for(var i=0; i<users.length; i++){
+     var user = users[i];
+     console.log(user.sayHello());
+}
+//console.log(users[0]);
+
+//console.log(user1.id, user1.name);
+//user1.sayHello();
+
+var message = new Object();
+message.text="Hello";
+console.log(message.text);
